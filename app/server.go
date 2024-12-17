e5c79118f544c76033f71c6473b06a2ef36c3a47
zerobytez <zerobytez@mail.com>
2024-12-17 16:12:21 +0700

    hard-coded correlation_id

--- app/server.go
+++ app/server.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+type Response struct {
+	MessageSize   uint32 // 4 byte
+	CorrelationId uint32 // 4 byte
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
@@ -16,9 +19,34 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	fmt.Println("Listening on port 9092")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	var response Response
+	response.CorrelationId = 7
+
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.BigEndian, response); err != nil {
+		fmt.Println("Error writing response:", err)
+		return
+	}
+
+	if _, err := conn.Write(buf.Bytes()); err != nil {
+		fmt.Println("Error sending response to client:", err)
+		return
 	}
 }
